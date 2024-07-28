4bbeafc0853304c05eaa1d6e87fb49775a25dcf8
bansaldhruv2779 <33120109+bansaldhruv2779@users.noreply.github.com>
2024-07-28 22:08:22 +0530

    Code cleanup (#15)
    * removed the unwanted code
    
    * removed the unwanted code
    
    * removed the unwanted code

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Layout from "./Layout";
 import Header from "./Header";
 import Footer from "./Footer";
-import Copyright from "./CopyrightText";
 
-export {Layout, Copyright, Header, Footer};
+export {Layout, Header, Footer};
