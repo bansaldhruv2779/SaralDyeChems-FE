ea61786e7f34be53c7570a4d4e4dd924bf81eaf1
bansaldhruv2779 <bansaldhruv2779@gmail.com>
2024-09-12 00:16:06 +0530

    navbar dropdown issue resolved

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -22,6 +22,26 @@ const Index = () => {
     }
   };
 
+  const handleLinkHover = link => {
+    // setActiveLink(link);
+    if (link === "Products") {
+      setShowProductsDropdown(true);
+    } else {
+      setShowProductsDropdown(false);
+      setMobileMenuOpen(false);
+    }
+  };
+
+  const handleLinkLeave = link => {
+    // setActiveLink(link);
+    if (link === "Products") {
+      setShowProductsDropdown(false);
+    } else {
+      setShowProductsDropdown(false);
+      setMobileMenuOpen(false);
+    }
+  };
+
   const handleMobileMenuToggle = () => {
     setMobileMenuOpen(!mobileMenuOpen);
   };
@@ -59,74 +79,73 @@ const Index = () => {
           {["Home", "Products", "About Us", "Our Team", "Contact Us"].map(
             link => (
               <div key={link} className="relative">
-                <a
-                  href={
-                    link === "Home"
-                      ? "/"
-                      : link === "About Us"
-                      ? "/about-us"
-                      : link === "Contact Us"
-                      ? "/contact-us"
-                      : link === "Our Team"
-                      ? "/#team"
-                      : "#"
-                  }
-                  onMouseOver={() => handleLinkClick(link)}
-                  onClick={() => handleLinkClick(link)}
-                  style={{fontSize: "12px"}}
-                  className={`text-sm pb-2 pl-2 pr-2 flex items-center ${
-                    activeLink === link
-                      ? "border-b-2 border-[#3C5D87] text-[#3C5D87]"
-                      : "border-none text-[#3D3D3D]"
-                  }`}>
-                  {link}
-                  {link === "Products" && (
-                    <KeyboardArrowDownIcon
-                      className={`ml-1 transition-transform duration-300 ${
-                        showProductsDropdown ? "rotate-180" : "rotate-0"
-                      }`}
-                    />
+                <div
+                  onMouseOver={() => handleLinkHover(link)}
+                  onClick={() => handleLinkClick(link)}>
+                  <a
+                    href={
+                      link === "Home"
+                        ? "/"
+                        : link === "About Us"
+                        ? "/about-us"
+                        : link === "Contact Us"
+                        ? "/contact-us"
+                        : link === "Our Team"
+                        ? "/#team"
+                        : "#"
+                    }
+                    style={{fontSize: "12px"}}
+                    className={`text-sm pb-2 pl-2 pr-2 flex items-center ${
+                      activeLink === link
+                        ? "border-b-2 border-[#3C5D87] text-[#3C5D87]"
+                        : "border-none text-[#3D3D3D]"
+                    }`}>
+                    {link}
+                    {link === "Products" && (
+                      <KeyboardArrowDownIcon
+                        className={`ml-1 transition-transform duration-300 ${
+                          showProductsDropdown ? "rotate-180" : "rotate-0"
+                        }`}
+                      />
+                    )}
+                  </a>
+                  {link === "Products" && showProductsDropdown && (
+                    <div
+                      className="absolute left-0 mt-2 rounded-md bg-white shadow-lg"
+                      onMouseLeave={() => handleLinkLeave(link)}>
+                      <a
+                        href="/products?category=Flat Bed Screen Printing Chemicals"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
+                        Flat bed Screen Printing Chemicals
+                      </a>
+                      <a
+                        href="/products?category=Rotary Screen Printing Chemicals"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
+                        Rotary Screen Printing Chemicals
+                      </a>
+                      <a
+                        href="/products?category=Screen Making Auxillaries"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
+                        Screen Making Auxillaries
+                      </a>
+                      <a
+                        href="/products?category=Digital Printing Chemicals"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
+                        Digital Printing Chemicals
+                      </a>
+                      <a
+                        href="/products?category=Textile Auxiliaries"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
+                        Textile Auxiliaries
+                      </a>
+                      <a
+                        href="/products?category=Textile Printing Machines"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
+                        Textile Printing Machinesaaaa
+                      </a>
+                    </div>
                   )}
-                </a>
-
-                {link === "Products" && showProductsDropdown && (
-                  <div
-                    className="absolute left-0 mt-2 rounded-md bg-white shadow-lg"
-                    onMouseLeave={e => {
-                      handleLinkClick(link);
-                    }}>
-                    <a
-                      href="/products?category=Flat Bed Screen Printing Chemicals"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
-                      Flat bed Screen Printing Chemicals
-                    </a>
-                    <a
-                      href="/products?category=Rotary Screen Printing Chemicals"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
-                      Rotary Screen Printing Chemicals
-                    </a>
-                    <a
-                      href="/products?category=Screen Making Auxillaries"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
-                      Screen Making Auxillaries
-                    </a>
-                    <a
-                      href="/products?category=Digital Printing Chemicals"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
-                      Digital Printing Chemicals
-                    </a>
-                    <a
-                      href="/products?category=Textile Auxiliaries"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
-                      Textile Auxiliaries
-                    </a>
-                    <a
-                      href="/products?category=Textile Printing Machines"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 whitespace-nowrap">
-                      Textile Printing Machines
-                    </a>
-                  </div>
-                )}
+                </div>
               </div>
             ),
           )}
